ba46e7a024b042ade6d21538c2ba4bb465359a23
sharonmaswai <chepmaswai@gmail.com>
2019-07-16 15:46:51 +0300

    register app in settings

--- icedtube/settings.py
+++ icedtube/settings.py
@@ -74,9 +74,11 @@ WSGI_APPLICATION = 'icedtube.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hood',
+        'USER': 'sharon-maswai',
+    'PASSWORD':'qwerty',
     }
 }
 
