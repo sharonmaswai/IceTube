5a072a0c45e2d8c114053d954122d248810288de
mitchnm <mitchndungu21@gmail.com>
2019-07-16 17:25:29 +0300

    created a navbar

--- iced/views.py
+++ iced/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    render(request, 'index.html')
+    return render(request, 'home/index.html')
--- icedtube/settings.py
+++ icedtube/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
      'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ice',
-        'USER': 'sharon-maswai',
-    'PASSWORD':'qwerty',
+        'USER': 'mitch',
+    'PASSWORD':'qwertyuiop',
     }
 }
 
