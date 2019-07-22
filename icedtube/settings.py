9f611abcd526798238f50a1e862d3b92bce95d61
Michael Korir <mikor22@gmail.com>
2019-07-22 14:12:54 +0300

    create token generator

--- icedtube/settings.py
+++ icedtube/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mikor22@gmail.com'
+EMAIL_HOST_PASSWORD = 'mikeware'
+EMAIL_PORT = 587
