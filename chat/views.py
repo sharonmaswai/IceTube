58be94bc2bab4c986fedde67cc4fce011945d202
sharonmaswai <chepmaswai@gmail.com>
2019-07-25 00:49:43 +0300

    replace html in room.html with chat.html

--- chat/views.py
+++ chat/views.py
@@ -10,7 +10,7 @@ User = get_user_model()
 
 def index(request):
     return render(request, 'chat/index.html', {})
-
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name)),
