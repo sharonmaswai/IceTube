55432c088fb3a151bf76eaa74dc0e7d1e63efcec
sharonmaswai <chepmaswai@gmail.com>
2019-07-25 00:45:30 +0300

    comment out view functions and import login required decorator

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
 from .models import Chat, Contact
 
@@ -18,15 +19,15 @@ def room(request, room_name):
 
 
 
-def get_last_12_messages(chatId):
-    chat = get_object_or_404(Chat, id=chatId)
-    return chat.messages.order_by('-timestamp').all()[:12]
+# def get_last_12_messages(chatId):
+#     chat = get_object_or_404(Chat, id=chatId)
+#     return chat.messages.order_by('-timestamp').all()[:12]
 
 
-def get_user_contact(username):
-    user = get_object_or_404(User, username=username)
-    return get_object_or_404(Contact, user=user)
+# def get_user_contact(username):
+#     user = get_object_or_404(User, username=username)
+#     return get_object_or_404(Contact, user=user)
 
 
-def get_current_chat(chatId):
-    return get_object_or_404(Chat, id=chatId)    
+# def get_current_chat(chatId):
+#     return get_object_or_404(Chat, id=chatId)    
