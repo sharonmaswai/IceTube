3ce8e07dafc7403513724b02c72559d4f2b1825a
mzambia70 <mzambia70@gmail.com>
2019-07-24 17:50:32 +0300

    added the view function for mpesa

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from pympesa import Pympesa
+import pympesa
 
 # Create your views here.
+
+response = pympesa.oauth_generate_token(
+   '''DRq4b3D5tNWw4Q3uUUiYQLCgabKNlZir,bDnSilPbQIidgcrM
+).json() '''
+"access_token"=response.get("x0Bng5LDe75F6G8grgm1AwSwOdJ3")
+
+mpesa_client.lipa_na_mpesa_online_payment(
+    BusinessShortCode="600000",
+    Password="xxxxx_yyyy_zzz",
+    TransactionType="CustomerPayBillOnline",
+    Amount="100",
+    PartyA="254708374149",
+    PartyB="600000",
+    PhoneNumber="254708374149",
+    CallBackURL="https://your-app/callback",
+    AccountReference="ref-001",
+    TransactionDesc="desc-001"
+    )
