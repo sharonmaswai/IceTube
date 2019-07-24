1655fe8c8a8b178541254d2ba77453b8765d6ffb
sharonmaswai <chepmaswai@gmail.com>
2019-07-24 18:09:54 +0300

    add receive function to consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -112,22 +112,22 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
 
-    # def receive(self, text_data):
-    #     data = json.loads(text_data)
-    #     self.commands[data['command']](self, data)
+    def receive(self, text_data):
+        data = json.loads(text_data)
+        self.commands[data['command']](self, data)
 
-    # def send_chat_message(self, message):
-    #     async_to_sync(self.channel_layer.group_send)(
-    #         self.room_group_name,
-    #         {
-    #             'type': 'chat_message',
-    #             'message': message
-    #         }
-    #     )
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
 
-    # def send_message(self, message):
-    #     self.send(text_data=json.dumps(message))
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
 
-    # def chat_message(self, event):
-    #     message = event['message']
-    #     self.send(text_data=json.dumps(message))
+    def chat_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps(message))
