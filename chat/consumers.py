e4b5c461c643fadab5aff55ab9e237d4a2676769
sharonmaswai <chepmaswai@gmail.com>
2019-07-24 18:07:48 +0300

    add disconnect function to consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -84,13 +84,13 @@ class ChatConsumer(WebsocketConsumer):
             result.append(self.message_to_json(message))
         return result
 
-    # def message_to_json(self, message):
-    #     return {
-    #         'id': message.id,
-    #         'author': message.contact.user.username,
-    #         'content': message.content,
-    #         'timestamp': str(message.timestamp)
-    #     }
+    def message_to_json(self, message):
+        return {
+            'id': message.id,
+            'author': message.contact.user.username,
+            'content': message.content,
+            'timestamp': str(message.timestamp)
+        }
 
     commands = {
         'fetch_messages': fetch_messages,
@@ -112,22 +112,22 @@ class ChatConsumer(WebsocketConsumer):
             self.channel_name
         )
 
-    def receive(self, text_data):
-        data = json.loads(text_data)
-        self.commands[data['command']](self, data)
+    # def receive(self, text_data):
+    #     data = json.loads(text_data)
+    #     self.commands[data['command']](self, data)
 
-    def send_chat_message(self, message):
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
+    # def send_chat_message(self, message):
+    #     async_to_sync(self.channel_layer.group_send)(
+    #         self.room_group_name,
+    #         {
+    #             'type': 'chat_message',
+    #             'message': message
+    #         }
+    #     )
 
-    def send_message(self, message):
-        self.send(text_data=json.dumps(message))
+    # def send_message(self, message):
+    #     self.send(text_data=json.dumps(message))
 
-    def chat_message(self, event):
-        message = event['message']
-        self.send(text_data=json.dumps(message))
+    # def chat_message(self, event):
+    #     message = event['message']
+    #     self.send(text_data=json.dumps(message))
