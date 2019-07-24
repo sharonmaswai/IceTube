3269bafad3b6326ead1ae06a29e012adfe8a7541
sharonmaswai <chepmaswai@gmail.com>
2019-07-25 00:42:52 +0300

    add username variable to room function in views.py

--- chat/views.py
+++ chat/views.py
@@ -12,7 +12,8 @@ def index(request):
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name))
+        'room_name_json': mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
     })
 
 
