29af318400622a6ebd4844f2b55184dd946383ba
mitchnm <mitchndungu21@gmail.com>
2019-07-25 18:22:06 +0300

    trying to fix login issues

--- icedtube/urls.py
+++ icedtube/urls.py
@@ -19,9 +19,9 @@ from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include ('iced.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}), 
-    url(r'', include ('iced.urls')),
     url(r'^daraja/', include('payments.urls')),
     url(r'^chat/', include('chat.urls')),
 ]
